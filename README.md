0930daa3b7bd0234fe68826e355b5fc4964e0efb
salms358 <sa083850@gmail.com>
2023-03-08 15:23:37 +0000

    images

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 Fun classic game of rock paper scissors
 ## Description
 The game is a basic game of Rock paper scissors in which the icons will glow either green, red or blue depending if you win lose or draw with the computer. Once the user or computer reaches 5 scores the round number increases by one. There are 2 rounds in the game the winner can enter in their email to recieve their scores via email. 
+
+ [Am I responsive](https://ui.dev/amiresponsive?url=https://8000-salms358-rockpapersciss-g2ikctro4ln.ws-eu89.gitpod.io/)
+ # Features:
+ ## Theme
+
+
+ ## Buttons
+  # User stories 
+  ## First-time user
+
+  ## Second-time user
+
+  ## Frequent-user
+
+  ## Testing
+  ## Validator testing
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,121 +1,121 @@
 
-const div_paper = document.getElementById("paper");
-const userScore_span = document.getElementById("player-score");
-const div_rock = document.getElementById("rock");
-const compScore_span = document.getElementById("comp-score");
-const score_board_div = document.getElementsByClassName(".scorz");
-const div_scissors = document.getElementById("scissors");
-const NumberOfRounds_span = document.getElementById("NumberOfRounds");
-let user_scores = 0;
-let comp_scores = 0;
-let Roundnumber = 0;
+    const div_paper = document.getElementById("paper");
+    const userScore_span = document.getElementById("player-score");
+    const div_rock = document.getElementById("rock");
+    const compScore_span = document.getElementById("comp-score");
+    const score_board_div = document.getElementsByClassName(".scorz");
+    const div_scissors = document.getElementById("scissors");
+    const NumberOfRounds_span = document.getElementById("NumberOfRounds");
+    let userScore = 0;
+    let compScore = 0;
+    let Roundnumber = 0;
 
-function ComputerChoice() {
-    const options = ['rock','paper','scissors'];
-    const ran_No = Math.floor(Math.random()*3);
-    console.log(ran_No)
-    return options[ran_No];
-}
-
-function NumberOfRounds(user, computer) {
-    if (user_scores == 5 || comp_scores == 5) {
-    Roundnumber++
- NumberOfRounds_span.innerHTML = Roundnumber;
-
- user_scores = 0;
- comp_scores = 0;
- userScore_span.textContent = user_scores;
- compScore_span.textContent = comp_scores;
- Outcome(`${user} wins the round against ${computer}`)
-
-}
-
-}
-
-function Outcome(result) {
-    document.getElementById('round-outcome').innerHTML = result;
-}
-function play_result(result) {
-    document.getElementById('result-text').innerHTML = result;
-}
-
- function removeBorder() {
-    let options = document.querySelectorAll(".option")
-    for (let i = 0; i < options.length; i++) {
-        options[i].classList.remove("glowing_green");
-        options[i].classList.remove("glowing_blue");
-        options[i].classList.remove("glowing_red");
+    function ComputerChoice() {
+        const options = ['rock','paper','scissors'];
+        const ran_No = Math.floor(Math.random()*3);
+        console.log(ran_No)
+        return options[ran_No];
     }
-    let optionsIcons = document.querySelectorAll(".option i")
-    for (let i = 0; i < optionsIcons.length; i++) {
-        optionsIcons[i].classList.remove("glowing_green");
-        optionsIcons[i].classList.remove("glowing_blue");
-        optionsIcons[i].classList.remove("glowing_red");
+
+    function NumberOfRounds(user, computer) {
+        if (userScore == 5 || compScore == 5) {
+        Roundnumber++
+    NumberOfRounds_span.innerHTML = Roundnumber;
+
+    userScore = 0;
+    compScore = 0;
+    userScore_span.textContent = userScore;
+    compScore_span.textContent = compScore;
+    Outcome(`${user} wins the round against ${computer}`)
+
     }
-}
+
+    }
+
+    function Outcome(result) {
+        document.getElementById('round-outcome').innerHTML = result;
+    }
+    function play_result(result) {
+        document.getElementById('result-text').innerHTML = result;
+    }
+
+    function removeBorder() {
+        let options = document.querySelectorAll(".option")
+        for (let i = 0; i < options.length; i++) {
+            options[i].classList.remove("glowing_green");
+            options[i].classList.remove("glowing_blue");
+            options[i].classList.remove("glowing_red");
+        }
+        let optionsIcons = document.querySelectorAll(".option i")
+        for (let i = 0; i < optionsIcons.length; i++) {
+            optionsIcons[i].classList.remove("glowing_green");
+            optionsIcons[i].classList.remove("glowing_blue");
+            optionsIcons[i].classList.remove("glowing_red");
+        }
+    }
       
 
-function win(user, computer) {
-    user_scores++
-    userScore_span.innerHTML = user_scores;
-    play_result(`${user} overpowers ${computer}, you won`);
-}
-    
-function draw() {
-    userScore_span.innerHTML = user_scores;
-    play_result(`Its a draw`);         
-}
+    function win(user, computer) {
+        userScore++
+        userScore_span.innerHTML = userScore;
+        play_result(`${user} overpowers ${computer}, you won`);
+    }
+        
+    function draw() {
+        userScore_span.innerHTML = userScore;
+        play_result(`Its a draw`);         
+    }
         
     
-function lose(user, computer) {
-    userScore_span.innerHTML = user_scores;
-    comp_scores++;
-    compScore_span.innerHTML = comp_scores;
-    play_result(`${computer} overpowers ${user}, you lost`);
+    function lose(user, computer) {
+        userScore_span.innerHTML = userScore;
+        compScore++;
+        compScore_span.innerHTML = compScore;
+        play_result(`${computer} overpowers ${user}, you lost`);
+        
+    }
     
-}
-  
 
 
-function rps(userChoice, pressedButton) {
-    const computerChoice = ComputerChoice();
-    console.log(`Computer plays ${computerChoice}`);
-    removeBorder();
-    switch (computerChoice + userChoice) {
-        case "paperscissors":
-        case "rockpaper":
-        case "scissorsrock":
-            pressedButton.classList.add("glowing_green");
-            win(userChoice, computerChoice);
-        break;
-        case "scissorspaper":
-        case "rockscissors":
-        case "paperrock":
-            lose(userChoice, computerChoice);
-            pressedButton.classList.add("glowing_red");
+    function rps(userChoice, pressedButton) {
+        const computerChoice = ComputerChoice();
+        console.log(`Computer plays ${computerChoice}`);
+        removeBorder();
+        switch (computerChoice + userChoice) {
+            case "paperscissors":
+            case "rockpaper":
+            case "scissorsrock":
+                pressedButton.classList.add("glowing_green");
+                win(userChoice, computerChoice);
             break;
-        case "rockrock":
-        case "paperpaper":
-        case "scissorsscissors":
-            draw()
-            pressedButton.classList.add("glowing_blue");
-            break;   
-    }
-    NumberOfRounds()
-}
-
-function round() {
-    
-    div_paper.addEventListener('click', function(event) {
-    rps("paper",  event.target)
-})
-    div_rock.addEventListener('click',  function(event) {
-    rps("rock", event.target)
-})
-    div_scissors.addEventListener('click',  function(event) {
-    rps("scissors", event.target)
-        })
-     
+            case "scissorspaper":
+            case "rockscissors":
+            case "paperrock":
+                lose(userChoice, computerChoice);
+                pressedButton.classList.add("glowing_red");
+                break;
+            case "rockrock":
+            case "paperpaper":
+            case "scissorsscissors":
+                draw()
+                pressedButton.classList.add("glowing_blue");
+                break;   
+        }
+        NumberOfRounds()
     }
 
-round();
+    function round() {
+        
+        div_paper.addEventListener('click', function(event) {
+        rps("paper",  event.target)
+    })
+        div_rock.addEventListener('click',  function(event) {
+        rps("rock", event.target)
+    })
+        div_scissors.addEventListener('click',  function(event) {
+        rps("scissors", event.target)
+            })
+        
+        }
+
+    round();
