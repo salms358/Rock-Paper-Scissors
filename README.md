4e5e79d09156cd5f760cf0e90e277f047fa2793c
salms358 <sa083850@gmail.com>
2023-03-11 19:50:22 +0000

    Added Media Queries and made sure functions and variables were camelCase

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Fun classic game of rock paper scissors
 ## Description
     The game is a basic game of Rock paper scissors in which the icons will glow either green, red or blue depending if you win lose or draw with the computer. Once the user or computer reaches 5 scores the round number increases by one. There are 2 rounds in the game the winner can enter in their email to recieve their scores via email. 
 
- [Am I responsive](https://ui.dev/amiresponsive?url=https://8000-salms358-rockpapersciss-g2ikctro4ln.ws-eu89.gitpod.io/)
+ [Am I responsive](https://ui.dev/amiresponsive?url=https://salms358.github.io/Rock-Paper-Scissors/)
  # Features:
  ## Theme
+ 
     The theme involves blue/green/grey colours which create a war-like theme as there is a battle between the user and the computer. 
     
     For the title I used grey as it matches with blue and it can be contrasted against so it can be easier to notice by the visually impaired
@@ -17,12 +18,17 @@ Fun classic game of rock paper scissors
     This will allow it to be more convinient with individuals who perhaps have dyslexia or any other problems with reading. 
     
     Additionally there is text displayed when the user wins, draws or loses and the fact that what they chose beats the computers choice.
-   
+  [Green-glow](https://mail.google.com/mail/u/0?ui=2&ik=318e7d6810&attid=0.1&permmsgid=msg-f:1760096400850960798&th=186d1e59e72a419e&view=att&disp=safe)
+
+  [Purple-glow](https://mail.google.com/mail/u/0?ui=2&ik=318e7d6810&attid=0.1&permmsgid=msg-f:1760096574353890183&th=186d1e824cbedb87&view=att&disp=safe)
+
+  [Red-glow](https://mail.google.com/mail/u/0?ui=2&ik=318e7d6810&attid=0.1&permmsgid=msg-f:1760096668110708688&th=186d1e9821165fd0&view=att&disp=safe)
 ## Form
     When the user wins the game a form will appear which allows the user to enter in their email and receive their scores via email. 
     
     I added this feature as it allows the user to keep track of their scores as they play or if they want to show their friends. 
-    
+  [image of email form](https://mail.google.com/mail/u/0?ui=2&ik=318e7d6810&attid=0.1&permmsgid=msg-f:1760095816233851336&th=186d1dd1c94519c8&view=att&disp=safe)
+  
 ## Captions
     -I added some captions one of them comparing the users and computers choice and who wins e.g."Rock overpowerd paper, you win". 
 
@@ -45,20 +51,57 @@ Fun classic game of rock paper scissors
     -Whilst playing I like how everything is easy to tell the difference between the icons. Its very easy to navigate around as the colours are contrasting.
 
   # Testing
-    The game seemed to be working the same way throughout the browsers Microsoft Edge, Mozilla Firefox and Google Chrome. The email text was working andlead to the scores of the user and computer to be sent to their email.
+    The game seemed to be working the same way throughout the browsers Microsoft Edge, Mozilla Firefox and Google Chrome. The email text was working and lead to the scores of the user and computer to be sent to their email.
+    The website was working the same across Microsoft edge, firefox and google chrome.
+
+The website has contrasting-colours so it is easier for users to read the information.
+
+The form is validated the submit button leads to the opening of the code institute 'congratulations' page. Moreover, if the user misses out on a field the form cannot be submitted and the user will be told to complete the field they missed out on for their form to be submitted.
+
 
   ## Validator testing
-
+      -Initially, my HTML included a lot of problems which I had to fix one by one a lot of the issues being to do with stray tags.
       - HTML validator showed no errors but 2 warnings about including type in script tags 
 
       - For CSS there were no errors or warnings.
 
       - For JS there were a lot of warnings but no sufficient problems.
       - My code obtained a high accessibility score of 97% and a overall score of 95% suggesting that users will find it easy to play the game.
-  [CSS validator](https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2F8000-salms358-rockpapersciss-g2ikctro4ln.ws-eu89b.gitpod.io%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en)
+  [CSS validator](http://jigsaw.w3.org/css-validator/validator?lang=en&profile=css3svg&uri=https%3A%2F%2Fsalms358.github.io%2FRock-Paper-Scissors%2F&usermedium=all&vextwarning=&warning=1)
 
-  [HTML validator](https://validator.w3.org/nu/?doc=https%3A%2F%2F8000-salms358-rockpapersciss-g2ikctro4ln.ws-eu89b.gitpod.io%2F)
+  [HTML validator](https://validator.w3.org/nu/?doc=https%3A%2F%2Fsalms358.github.io%2FRock-Paper-Scissors%2F)
 
 [Light house test](https://lighthouse-metrics.com/lighthouse/checks/280bdd1f-c65d-4e32-a48e-6eed5c1d5cae)
 
+# Fixed Bugs
+-One bug involved the buttons of the website not responding once they were clicked which was resolved by removing a misplaced semicolon.
+- The combinations of the options included in the switch-case were the wrong way around causing the user and computer scores to be interpreted incorrectly.
+# Known bugs
+-One of the buttons  move slightly out of place when the screen is set to tablet format. But at times this problem does not show up.
+# Deployment 
+1.Got to settings in Github repository.
+2.Select main-branch from the source drop-down menu.
+3.Once selected code for the website should be provided.
+# Languages
+1. HTML
+2. CSS 
+3. Javascript
+# Links
+Am I Responsive https://ui.dev/amiresponsive - Used to verify responsiveness of website on different devices.
 
+Responsinator - http://www.responsinator.com/ Used to verify the responsiveness of the website on different devices.
+
+YouTube video - https://www.youtube.com/watch?v=jaVNP3nIAv0&ab_channel=freeCodeCamp.org
+
+Chrome Developer Tools - Used for changing details and testing responsiveness accessed by pressing f12.
+
+Font Awesome - https://fontawesome.com/ Hand icons in footer.
+
+Google Fonts - https://fonts.google.com/ Used to import fonts on the website.
+
+Responsinator - http://www.responsinator.com/ Used to verify the responsiveness of the website on different devices.
+# Credits
+ ### W3 schools [website](https://www.w3schools.com/)
+ ### Stack Overflow [link](https://stackoverflow.com/)
+ ### YouTube [video](https://www.youtube.com/watch?v=jaVNP3nIAv0&ab_channel=freeCodeCamp.org)
+ ### Free Code Camps YouTube Channel
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
+  
+  document.addEventListener('DOMContentLoaded', function() {
   /*Defining all of the variables needed in the project including the scores and the displaying of the scores*/
-    const div_paper = document.getElementById("paper");
-    const userScore_span = document.getElementById("player-score");
-    const div_rock = document.getElementById("rock");
+    const divPaper = document.getElementById("paper");
+    const userScorespan = document.getElementById("player-score");
+    const divRock = document.getElementById("rock");
     const compScore_span = document.getElementById("comp-score");
     const score_board_div = document.getElementsByClassName(".scorz");
-    const div_scissors = document.getElementById("scissors");
-    const NumberOfRounds_span = document.getElementById("NumberOfRounds");
+    const divScissors = document.getElementById("scissors");
+    const numberOfRoundsspan = document.getElementById("NumberOfRounds");
     /*All of the scores involved in the game*/
     let userScore = 0;
     let compScore = 0;
     let Roundnumber = 0;
-    let UserScoreEmail = 0;
-    let CompScoreEmail = 0;
+    let userScoreEmail = 0;
+    let compScoreEmail = 0;
    
  /*generates a random choice from the computer so that it can be later compared with the user scores to determine who wins the round*/
-    function ComputerChoice() {
+    function  computerChoices() {
         const options = ['rock','paper','scissors'];
         const ran_No = Math.floor(Math.random()*3);
         console.log(ran_No);
@@ -22,40 +24,40 @@
     }
 /*Makes sure the Round number is increased, displays the statement showing who won and the amount of rounds it took for either the user or the computer to win. Moreover this function 
 involves the form being hidden for the duraation of the game until the user is the winner*/
-    function NumberOfRounds() {
+    function numberOfRounds() {
         Roundnumber++;
         document.getElementById('form').classList.add('hide')
-        NumberOfRounds_span.innerHTML = Roundnumber
+        numberOfRoundsspan.innerHTML = Roundnumber
         if (userScore == 5 || compScore == 5)  {
             if (userScore == 5) {
-                Outcome(`User wins against Computer ${userScore} to ${compScore} in ${Roundnumber} rounds`)
+                outCome(`User wins against Computer ${userScore} to ${compScore} in ${Roundnumber} rounds`)
                 document.getElementById('form').classList.remove('hide')
             }
             else{
-                Outcome(`Computer wins against User ${compScore} to ${userScore} in ${Roundnumber} rounds`)
+                outCome(`Computer wins against User ${compScore} to ${userScore} in ${Roundnumber} rounds`)
             }
-            UserScoreEmail = userScore;
-            CompScoreEmail = compScore;
+            userScoreEmail = userScore;
+            compScoreEmail = compScore;
             Roundnumber = 0;
             userScore = 0;
             compScore = 0;
            /*Makes sure the round number and scores are shown for limited time until the scores reset*/
             setTimeout(function() {
-                userScore_span.textContent = userScore;
+                userScorespan.textContent = userScore;
                 compScore_span.textContent = compScore;
-                NumberOfRounds_span.innerHTML = Roundnumber;
+                numberOfRoundsspan.innerHTML = Roundnumber;
                 document.getElementById('round-outcome').classList.add('hide')
             }, 10000);
         }
 
     }
 /*The functions that is called to display the round outcome*/
-    function Outcome(result) {
+    function outCome(result) {
         document.getElementById('round-outcome').classList.remove('hide');
         document.getElementById('round-outcome').innerHTML = result;
     }
     /*Is called to determine how the user wins loses or draws e.g.rock overpowers paper, you win*/
-    function play_result(result) {
+    function playResult(result) {
         document.getElementById('result-text').innerHTML = result;
     }
 /*Makes sure the borders of the icons stop glowing after another option is clicked*/
@@ -79,8 +81,8 @@ involves the form being hidden for the duraation of the game until the user is t
       console.log("CompScoreEmail, UserScoreEmail, userScore, compScore", CompScoreEmail, UserScoreEmail, userScore, compScore);
             let templateParams = {
                 user_email: document.getElementById('email_address').value,
-                score: UserScoreEmail,
-                compScore: CompScoreEmail,
+                score: userScoreEmail,
+                compScore: compScoreEmail,
             };
         
         emailjs.send('service_3otnutb', 'template_cebba9n', templateParams)
@@ -95,29 +97,29 @@ involves the form being hidden for the duraation of the game until the user is t
  as drawing does not impact the score*/
     function win(user, computer) {
         userScore++
-        userScore_span.innerHTML = userScore;
-        play_result(`${user} overpowers ${computer}, you won`);
+        userScorespan.innerHTML = userScore;
+        playResult(`${user} overpowers ${computer}, you won`);
     }
         
     function draw() {
-        userScore_span.innerHTML = userScore;
-        play_result(`Its a draw`);         
+        userScorespan.innerHTML = userScore;
+        playResult(`Its a draw`);         
     }
         
     
     function lose(user, computer) {
-        userScore_span.innerHTML = userScore;
+        userScorespan.innerHTML = userScore;
         compScore++;
         compScore_span.innerHTML = compScore;
-        play_result(`${computer} overpowers ${user}, you lost`);
+        playResult(`${computer} overpowers ${user}, you lost`);
         
     }
     
 
 /*Shows all of the possble choices that the user and computer makes it allows their choices to be compared. The 
 win draw and lose functions are called here to ensure that the program knows which combinations have the outcome of win, draw or lose*/
-    function rps(userChoice, pressedButton) {
-        const computerChoice = ComputerChoice();
+    function rockpaperScissors(userChoice, pressedButton) {
+        const computerChoice = computerChoices();
         console.log(`Computer plays ${computerChoice}`);
         removeBorder();
         switch (computerChoice + userChoice) {
@@ -140,25 +142,28 @@ win draw and lose functions are called here to ensure that the program knows whi
                 pressedButton.classList.add("glowing_blue");
                 break;   
         }
-        /*The number of rounds is called here to make sure that that after the scores are updated the round number should go up by 1*/
-        NumberOfRounds()
+     /*The number of rounds is called here to make sure that that after the scores are updated the round number should go up by 1*/
+     numberOfRounds()
     }
-/*Identifies the button clicked e.g. when i click rock the computer knows ive clicke the rock button*/
-    function round() {
-        div_paper.addEventListener('click', function(event) {
-        rps("paper",  event.target)
+   /*Identifies the button clicked e.g. when i click rock the computer knows ive clicke the rock button*/
+    function clickButton() {
+        divPaper.addEventListener('click', function(event) {
+            rockpaperScissors("paper",  event.target)
     })
-        div_rock.addEventListener('click',  function(event) {
-        rps("rock", event.target)
+        divRock.addEventListener('click',  function(event) {
+            rockpaperScissors("rock", event.target)
     })
-        div_scissors.addEventListener('click',  function(event) {
-        rps("scissors", event.target)
+        divScissors.addEventListener('click',  function(event) {
+            rockpaperScissors("scissors", event.target)
             })
         
         }
 
-    round()
- /*Makessure the email is submitted to the users email*/
+        clickButton()
+ /*Makes sure the email is submitted to the users email*/
     document.getElementById('submit').addEventListener('click', function(){
         sendEmail()
     });
+
+    console.log('Document is ready!')
+});
